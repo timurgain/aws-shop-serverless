56da9473eb5341ee57e77e3e7151b2fdf7702ade
Timur Gain <timur.gain@gmail.com>
2024-06-23 18:50:19 +0400

    feat: add db transactional write at create_product lambda

--- aws_shop_serverless/lambda_functions/create_product.py
+++ aws_shop_serverless/lambda_functions/create_product.py
@@ -3,7 +3,7 @@ import http
 import uuid
 from decimal import Decimal
 from utils.constants import headers_mutating_methods
-from utils.dynamodb import initialize_dynamodb_tables
+from utils.dynamodb import get_products_table_name, get_stocks_table_name, execute_transact_write
 from utils.managers import join_product_stock
 from utils.logging import log_request
 from utils.errors import BadRequestError
@@ -14,10 +14,6 @@ def lambda_handler(event, context):
     log_request(event)
 
     try:
-        # 0. Init
-
-        products_table, stocks_table = initialize_dynamodb_tables()
-
         # 1. Read request body
 
         try:
@@ -34,34 +30,50 @@ def lambda_handler(event, context):
         if not title or not description or not price:
             raise BadRequestError("Invalid request body: title, description and price are required")
         
-        if not isinstance(price, (int, float, Decimal)):
-            raise BadRequestError("Invalid request body: price should be a number")
+        if not isinstance(price, (int, float, Decimal)) or not price > 0:
+            raise BadRequestError("Invalid request body: price should be a positive number")
         
-        if not isinstance(count, int):
-            raise BadRequestError("Invalid request body: count should be an integer")
+        if not isinstance(count, int) or not count >= 0:
+            raise BadRequestError("Invalid request body: count should be an integer that is greater or equal to 0")
 
-        # 2. Create product and stock
+        # 2. Prepare product data and stock data for DynamoDB TransactWriteItems operation
 
         product = {
+            'Put': {
+                "TableName": get_products_table_name(),
+                "Item": {
+                    "id": {"S": product_id},
+                    "title": {"S": title},
+                    "description": {"S": description},
+                    "price": {"N": str(price)},
+                }
+            }
+        }
+
+        stock = {
+            'Put': {
+                "TableName": get_stocks_table_name(),
+                "Item": {
+                    "product_id": {"S": product_id},
+                    "count": {"N": str(count)},
+                }
+            }
+        }
+
+        # 3. Put product and stock data into DynamoDB using TransactWriteItems operation
+        
+        execute_transact_write([product, stock])
+
+        # 4. Response
+
+        joined_product_data = {
             "id": product_id,
             "title": title,
             "description": description,
             "price": str(price),
-        }
-
-        stock = {
-            "product_id": product_id,
             "count": str(count),
         }
 
-        # 3. Put product and stock into tables
-
-        products_table.put_item(Item=product)
-        stocks_table.put_item(Item=stock)
-
-        joined_product_data = join_product_stock(product, [stock]) 
-
-        # 4. Response
         return {
             "headers": headers_mutating_methods,
             "statusCode": http.HTTPStatus.CREATED,
