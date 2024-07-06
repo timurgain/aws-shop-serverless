5de9254869eb87a6967042f99bb2f85aa6db4ee2
Timur Gain <timur.gain@gmail.com>
2024-07-06 18:47:27 +0400

    feat: catalog_batch_process lambda creates products using sqs messages

--- product_service/product_service/lambda_functions/catalog_batch_process.py
+++ product_service/product_service/lambda_functions/catalog_batch_process.py
@@ -1,11 +1,7 @@
 import json
-import http
-from utils.serializers import DecimalEncoder
-from utils.constants import headers_safe_methods
-from utils.managers import join_product_stock
-from utils.dynamodb import initialize_dynamodb_tables
+import uuid
+from utils.dynamodb import execute_transact_write, prepare_product_stock_data
 from utils.logging import log_request, log_info
-from utils.errors import NotFoundError
 
 
 def lambda_handler(event, context):
@@ -13,9 +9,31 @@ def lambda_handler(event, context):
     log_request(event)
 
     try:
-        for message in event.get('Records', []):
-            message_body = json.loads(message['body'])
-            log_info(f"Message body: {message_body}") 
+        for message in event.get("Records", []):
+
+            # 1. Loop through SQS messages
+
+            message_body = json.loads(message["body"])
+
+            product_id = str(uuid.uuid4())
+            title = message_body.get("title")
+            description = message_body.get("description")
+            price = int(message_body.get("price"))
+            count = int(message_body.get("count", 0))
+
+             # 2. Prepare product and stock
+
+            product, stock = prepare_product_stock_data(
+                product_id, title, description, price, count
+            )
+
+            # 3. Put product and stock data into DynamoDB using TransactWriteItems operation
+
+            execute_transact_write([product, stock])
+
+            # 4. Log the processed message
+
+            log_info(f"Processed message: {message_body}")
 
     except Exception as err:
         log_info(f"Error in catalog_batch_process: {err}")
